71a8e4aaedeef81571c264a4679483d9a1e8fdb8
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-03-04 12:20:13 +0530

    Added minio support to store uploaded binaries

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/minio/minio-go"
 	"io"
+	"log"
 	"net/http"
 )
 
+var minioClient *minio.Client
+var err error
+
 func main() {
+
+	//Test bed values. Replace with real minio address and keys
+	endpoint := "play.minio.io:9000"
+	accessKeyID := "Q3AM3UQ867SPQQA43P2F"
+	secretAccessKey := "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
+	useSSL := true
+
+	minioClient, err = minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/getbinary", UserBinaryHandler)
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
@@ -17,11 +33,36 @@ func main() {
 
 func UserBinaryHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(32 << 20)
-	binary, _, err := r.FormFile("upload")
+	binary, header, err := r.FormFile("upload")
 
 	if err != nil {
 		panic(err)
 	}
 
+	bucketName := "binary"
+	location := "us-east-1" //As given in docs. Might change when we use our own server
+
+	err = minioClient.MakeBucket(bucketName, location)
+	if err != nil {
+		// Check to see if we already own this bucket (which happens if you run this twice)
+		exists, err := minioClient.BucketExists(bucketName)
+		if err == nil && exists {
+			log.Printf("We already own %s\n", bucketName)
+		} else {
+			log.Fatalln(err)
+		}
+	}
+	log.Printf("Successfully created %s\n", bucketName)
+
+	objectName := header.Filename
+	contentType := "application/octet-stream"
+
+	// Upload the zip file with FPutObject
+	n, err := minioClient.PutObject(bucketName, objectName, binary, contentType)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", objectName, n)
 	io.Copy(w, binary)
 }
