26d94f8598b00010885543cb3ff536ba0e455347
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-03-09 17:18:51 +0530

    added auth.go

---
+++ auth.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+)
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+
+	username := r.Form["name"][0]
+	password := r.Form["password"][0]
+
+	err = authorize(username, password)
+
+	if err == nil {
+		cookie := &http.Cookie{Name: "rcs", Value: username, MaxAge: 3600, Secure: false, HttpOnly: true, Raw: username}
+		http.SetCookie(w, cookie)
+		w.Write([]byte("authenticated"))
+	} else {
+		w.Write([]byte("Wrong password"))
+	}
+}
+
+func SignUpHandler(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+
+	if err != nil {
+		panic(err)
+	}
+
+	username := r.Form["name"][0]
+	password := r.Form["password"][0]
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	_, err = DB.Exec("insert into login values($1, $2)", username, hashedPassword)
+	http.Redirect(w, r, "/", 302)
+}
+
+func authorize(username string, password string) (autherr error) {
+
+	var dbpassword string
+
+	rows, err := DB.Query("Select password from login where name=$1", username)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		err = rows.Scan(&dbpassword)
+
+		if err != nil {
+			panic(err)
+		}
+
+		break
+
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(dbpassword), []byte(password))
+
+	if err == nil {
+
+		return nil
+
+	} else {
+		autherr = fmt.Errorf("Cannot authorize %q", username)
+
+		return autherr
+	}
+
+}
